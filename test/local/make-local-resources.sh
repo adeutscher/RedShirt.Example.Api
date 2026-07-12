fd3924078fb414d4b36d50deb7396597adc33145
Alan Deutscher <adeutscher@gmail.com>
2026-07-12 13:07:11 -0700

    Switch from localstack to ministack to avoid telemetry

--- test/local/make-local-resources.sh
+++ test/local/make-local-resources.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-awslocal dynamodb create-table --table-name example --attribute-definitions AttributeName=Name,AttributeType=S --key-schema AttributeName=Name,KeyType=HASH --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10
+AWS_DEFAULT_REGION=us-east-1 awslocal dynamodb create-table \
+    --table-name example \
+    --attribute-definitions AttributeName=Name,AttributeType=S \
+    --key-schema AttributeName=Name,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10
